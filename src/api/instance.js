35262b2ba81ca8088e2b9f6b82a9b3b7b068f1ee
lyhongkhanh <khanh.lyhong.it@gmail.com>
2025-07-12 21:06:49 +0700

    init src

---
+++ src/api/instance.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+// const baseURL = "https://social-networking-api.up.railway.app";
+// const baseURL = "http://localhost:8888";
+const baseURL = "https://sab-be.onrender.com";
+// const basePushNotificationURL =
+//   "https://familycircle-firebaseserver.onrender.com";
+export const instance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  params: {},
+  withCredentials: true,
+  timeout: 10000, // Set timeout to 10 seconds
+});
+instance.interceptors.response.use(
+  function (response) {
+    if (response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+export const instanceFile = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+  params: {},
+  withCredentials: true,
+  timeout: 10000, // Set timeout to 10 seconds
+});
+instanceFile.interceptors.response.use(
+  function (response) {
+    if (response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+// export const instancePushNotification = axios.create({
+//   baseURL: basePushNotificationURL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   params: {},
+// });
+// instancePushNotification.interceptors.response.use(
+//   function (response) {
+//     if (response.data) {
+//       return response.data;
+//     }
+//     return response;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   }
+// );
